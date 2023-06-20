37b228117a694758e5fc83e03caf169d763cecfd
SantiagoPulgarin415 <lgsantiago516@gmail.com>
2023-06-19 21:25:44 -0500

    Cambio en API

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/user/', (req, res)=>{
+routes.get('/cliente/', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
 
-        conn.query('SELECT * FROM usuarios',(err, rows)=>{
+        conn.query('SELECT * FROM clientes',(err, rows)=>{
             if(err) return res.send(err)
 
             res.json(rows)
@@ -13,11 +13,11 @@ routes.get('/user/', (req, res)=>{
     })
 })
 
-routes.post('/user/', (req, res)=>{
+routes.post('/cliente/', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
 
-        conn.query('INSERT INTO usuarios set ?',[req.body],(err, rows)=>{
+        conn.query('INSERT INTO clientes set ?',[req.body],(err, rows)=>{
             if(err) return res.send(err)
 
             res.send('person inserted!')
@@ -25,11 +25,11 @@ routes.post('/user/', (req, res)=>{
     })
 })
 
-routes.delete('/user/:id', (req, res)=>{
+routes.delete('/cliente/:id', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
 
-        conn.query('DELETE FROM usuarios WHERE id=?',[req.params.id],(err, rows)=>{
+        conn.query('DELETE FROM clientes WHERE id=?',[req.params.id],(err, rows)=>{
             if(err) return res.send(err)
 
             res.send('person excluded!')
@@ -37,11 +37,11 @@ routes.delete('/user/:id', (req, res)=>{
     })
 })
 
-routes.put('/user/:id', (req, res)=>{
+routes.put('/cliente/:id', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
 
-        conn.query('UPDATE usuarios set ? WHERE id = ?',[req.body,req.params.id],(err, rows)=>{
+        conn.query('UPDATE clientes set ? WHERE id = ?',[req.body,req.params.id],(err, rows)=>{
             if(err) return res.send(err)
 
             res.send('person updated!')
@@ -49,6 +49,41 @@ routes.put('/user/:id', (req, res)=>{
     })
 })
 
+
+routes.post('/compras/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        const hoy = new Date();
+        let total = 0;
+        let productId;
+
+        req.body.forEach(item => {
+            total += item.subtotal;
+        });
+
+        const compraJson = {
+            precio_final: total,
+            fecha_compra: hoy.toLocaleDateString()
+        };
+
+        conn.query('INSERT INTO compras set ?',[compraJson],(err, rows)=>{
+            if(err) return res.send(err)
+            productId = rows.insertId;
+            req.body.forEach(item => {
+                const compraRepuestoJson = {
+                    id_compra_fk: productId,
+                    ...item,
+                };
+                conn.query('INSERT INTO compra_x_repuesto set ?',[compraRepuestoJson],(err, rows)=>{
+                    if(err) return res.send(err)
+                })
+            });
+        })
+
+        res.send('datos guardados')
+    })
+})
 
 
 
